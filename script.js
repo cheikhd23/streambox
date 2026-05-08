619c7a2a0e896d6fd136e7ce0916208718181133
Cheikh Ahmed Tidiane Dione <colbyvision1@gmail.com>
2026-05-08 15:01:14 +0000

    Add Responsive Web Design RWD

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // StreamBox frontend scripts go here.
+const hamburger = document.getElementById('hamburger');
+const siteNav = document.getElementById('siteNav');
+
+if (hamburger && siteNav) {
+    hamburger.addEventListener('click', () => {
+        siteNav.classList.toggle('open');
+    });
+}
